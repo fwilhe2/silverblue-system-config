a86a5f7da17d56a14e14c11022e080daca995410
Florian Wilhelm <florian@linux.fritz.box>
2018-12-29 16:03:15 +0100

    Add simple script for package install

---
+++ install-packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+rpm-ostree install vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv thunderbird keepassxc java-11-openjdk-dev
+flatpak install flathub com.visualstudio.code
+flatpak install flathub org.freedesktop.Sdk.Extension.openjdk11
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+
