5a9af3ac5a73ae3b64d9e9fc5166bb2bcff11c6b
Florian Wilhelm <fwilhelm.wgt@gmail.com>
2018-12-30 13:13:14 +0100

    Use idempotent flag and fix package name

--- install-packages.sh
+++ install-packages.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rpm-ostree install vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv thunderbird keepassxc java-11-openjdk-dev xclip
+rpm-ostree install --idempotent vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv thunderbird keepassxc java-11-openjdk-devel xclip
 
 flatpak install flathub com.visualstudio.code
 flatpak install flathub org.freedesktop.Sdk.Extension.openjdk11
 flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+flatpak install flathub org.gimp.GIMP
