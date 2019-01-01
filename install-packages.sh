fe3cfe792f0b517958afa2071743140d88a95df7
Florian Wilhelm <fwilhelm.wgt@gmail.com>
2019-01-01 11:00:43 +0100

    Assume yes on flatpak install

--- install-packages.sh
+++ install-packages.sh
@@ -2,7 +2,8 @@
 
 rpm-ostree install --idempotent vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv thunderbird keepassxc java-11-openjdk-devel xclip
 
-flatpak install flathub com.visualstudio.code
-flatpak install flathub org.freedesktop.Sdk.Extension.openjdk11
-flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
-flatpak install flathub org.gimp.GIMP
+flatpak install --assumeyes flathub com.visualstudio.code
+flatpak install --assumeyes flathub org.freedesktop.Sdk.Extension.openjdk11
+flatpak install --assumeyes flathub com.jetbrains.IntelliJ-IDEA-Community
+flatpak install --assumeyes flathub org.gimp.GIMP
+
