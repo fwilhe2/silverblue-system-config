b4f0b5e3689968b700c6a65307891339a001cc38
Florian Wilhelm <fwilhelm.wgt@gmail.com>
2019-02-27 14:54:20 -0800

    update

--- install-chrome.sh
+++ install-chrome.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+# Hacky workaround to get google chrome working on Fedora Silverblue
+# Getting chromium is easy, but does not provide media codecs required for many nice things
+# See https://github.com/projectatomic/rpm-ostree/issues/233 for details
 
-wget --output-file=/tmp/chrome.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-su -
-rpm2cpio /tmp/chrome.rpm | (cd / && cpio -div)
+echo Chrome version before script
+/opt/google/chrome/google-chrome --version
+
+curl https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm > /tmp/chrome.rpm
+rpm2cpio /tmp/chrome.rpm | (cd / && cpio -div) > /dev/null
+
+echo Chrome version after script
+/opt/google/chrome/google-chrome --version
 
