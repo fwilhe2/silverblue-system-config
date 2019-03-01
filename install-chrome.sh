d02b84e7175d77e4796ee5b299ac96451bf4863b
Florian Wilhelm <fwilhelm.wgt@gmail.com>
2019-03-01 11:08:07 -0800

    Chrome desktop icon

--- install-chrome.sh
+++ install-chrome.sh
@@ -9,6 +9,8 @@ echo Chrome version before script
 curl https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm > /tmp/chrome.rpm
 rpm2cpio /tmp/chrome.rpm | (cd / && cpio -div) > /dev/null
 
+cp -f Chrome.desktop ~/.local/share/applications/Chrome.desktop
+
 echo Chrome version after script
 /opt/google/chrome/google-chrome --version
 
