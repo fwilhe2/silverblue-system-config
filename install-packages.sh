1e9d285719a9c0bb65cb65a2348d29d89592f083
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-08-06 17:31:07 +0200

    Update config

--- install-packages.sh
+++ install-packages.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-rpm-ostree install --idempotent vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv thunderbird keepassxc java-11-openjdk-devel xclip libXScrnSaver
+rpm-ostree install --idempotent vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv keepassxc java-11-openjdk-devel xclip libXScrnSaver
 
-flatpak install --assumeyes flathub com.visualstudio.code
-flatpak install --assumeyes flathub org.freedesktop.Sdk.Extension.openjdk11
-flatpak install --assumeyes flathub com.jetbrains.IntelliJ-IDEA-Community
+flatpak install --assumeyes flathub com.visualstudio.code-oss
 flatpak install --assumeyes flathub org.gimp.GIMP
-flatpak install --assumeyes flathub org.gnome.Eolie
