f7a7942e7761facbd44d3bcf5c1bf795994a99f5
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2022-11-27 19:03:16 +0100

    Use chrome from flathub

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-rpm-ostree install --idempotent vim zsh htop mc autojump autojump-zsh httpie the_silver_searcher jq git make direnv keepassxc java-11-openjdk-devel xclip libXScrnSaver
+rpm-ostree install --idempotent vim zsh htop autojump autojump-zsh httpie the_silver_searcher jq git keepassxc xclip libXScrnSaver
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install --assumeyes flathub com.visualstudio.code-oss
 flatpak install --assumeyes flathub org.gimp.GIMP
+flatpak install --assumeyes flathub com.google.Chrome
