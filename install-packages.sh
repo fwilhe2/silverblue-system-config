ecdc45c8c8418f31bc9431293fa5f0eb26329bdd
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-11-27 20:48:23 +0100

    disable flatpak for now

--- install-packages.sh
+++ install-packages.sh
@@ -2,8 +2,8 @@
 
 rpm-ostree install --allow-inactive --idempotent vim zsh htop autojump autojump-zsh httpie the_silver_searcher jq git keepassxc xclip libXScrnSaver
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install --assumeyes flathub com.visualstudio.code-oss
-flatpak install --assumeyes flathub org.gimp.GIMP
-flatpak install --assumeyes flathub com.google.Chrome
+# flatpak install --assumeyes flathub com.visualstudio.code-oss
+# flatpak install --assumeyes flathub org.gimp.GIMP
+# flatpak install --assumeyes flathub com.google.Chrome
